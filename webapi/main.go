14d0062da96c4dcaf169886e44445d2586752012
Teroaz <phsarafian@gmail.com>
2025-01-28 14:58:27 +0100

    error path

--- webapi/main.go
+++ webapi/main.go
@@ -1,51 +1,57 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 type whoami struct {
-	Name  string
-	Title string
-	State string
+    Name  string
+    Title string
+    State string
 }
 
 func main() {
-	request1()
+    request1()
 }
 
 func whoAmI(response http.ResponseWriter, r *http.Request) {
-	who := []whoami{
-		whoami{Name: "Les bests du devops et les 2 autres",
-			Title: "DevOps and Continous Deployment",
-			State: "FR",
-		},
-	}
+    who := []whoami{
+        {
+            Name:  "Les bests du devops et les 2 autres",
+            Title: "DevOps and Continuous Deployment",
+            State: "FR",
+        },
+    }
 
-	json.NewEncoder(response).Encode(who)
-
-	fmt.Println("Endpoint Hit", who)
+    json.NewEncoder(response).Encode(who)
+    fmt.Println("Endpoint Hit: /whoami")
 }
 
 func homePage(response http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(response, "Welcome to the Web API!")
-	fmt.Println("Endpoint Hit: homePage")
+    fmt.Fprintf(response, "Welcome to the Web API!")
+    fmt.Println("Endpoint Hit: /")
 }
 
 func aboutMe(response http.ResponseWriter, r *http.Request) {
-	who := "EfreiParis"
+    who := "EfreiParis"
+    fmt.Fprintf(response, "A little bit about me...")
+    fmt.Println("Endpoint Hit: /aboutme ->", who)
+}
 
-	fmt.Fprintf(response, "A little bit about me...")
-	fmt.Println("Endpoint Hit: ", who)
+func errorHandler(response http.ResponseWriter, r *http.Request) {
+    http.Error(response, "Une erreur est survenue", http.StatusInternalServerError)
+    fmt.Println("Endpoint Hit: /error (erreur générée)")
 }
 
 func request1() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/aboutme", aboutMe)
-	http.HandleFunc("/whoami", whoAmI)
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/aboutme", aboutMe)
+    http.HandleFunc("/whoami", whoAmI)
+    
+    http.HandleFunc("/error", errorHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
